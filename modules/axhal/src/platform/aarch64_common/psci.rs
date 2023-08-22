a133e72e02af7185eef4528f63fe33e882a19960
elliott10 <xiaoluoyuan@163.com>
2023-08-22 14:13:58 +0800

    Modified psci call

--- modules/axhal/src/platform/aarch64_common/psci.rs
+++ modules/axhal/src/platform/aarch64_common/psci.rs
@@ -46,17 +46,24 @@ fn psci_hvc_call(func: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     ret
 }
 
-fn psci_call(fn_id: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
-    match axconfig::PSCI_METHOD {
+fn psci_call(fn_id: usize, arg0: usize, arg1: usize, arg2: usize) -> Result<i32, PsciRetNo> {
+    let ret = match axconfig::PSCI_METHOD {
         "smc" => arm_smccc_smc(fn_id, arg0, arg1, arg2),
         "hvc" | _ => psci_hvc_call(fn_id, arg0, arg1, arg2),
+    };
+
+    let retno = psci_errno_tran(ret as i32);
+    if retno == PsciRetNo::PsciRetSuccess {
+        Ok(0)
+    } else {
+        Err(retno)
     }
 }
 
 /// Shutdown the whole system, including all CPUs.
 pub fn system_off() -> ! {
     info!("Shutting down...");
-    psci_call(PSCI_0_2_FN_SYSTEM_OFF as usize, 0, 0, 0);
+    let _ = psci_call(PSCI_0_2_FN_SYSTEM_OFF as usize, 0, 0, 0);
     warn!("It should shutdown!");
     loop {
         crate::arch::halt();
@@ -71,13 +78,17 @@ pub fn system_off() -> ! {
 /// cpu entry_point: phys_addr(secondary_entry)
 pub fn psci_cpu_on(cpuid: usize, entry_point: usize, arg: usize) -> i32 {
     info!("Starting CPU {} ON ...", cpuid);
-    let err = psci_0_1_cpu_on(PSCI_0_2_FN64_CPU_ON, cpuid, entry_point, arg);
-    if err != 0 {
-        error!("failed to boot CPU {} ({})", cpuid, err);
-    } else {
-        info!("Started CPU {}", cpuid);
+    let err = psci_call(PSCI_0_2_FN64_CPU_ON as usize, cpuid, entry_point, arg);
+    match err {
+        Err(e) => {
+            error!("failed to boot CPU {} ({:?})", cpuid, e);
+            e as i32
+        }
+        Ok(_) => {
+            info!("Started CPU {}", cpuid);
+            0
+        }
     }
-    err
 }
 
 pub fn psci_cpu_off(_cpuid: usize) {
@@ -85,84 +96,60 @@ pub fn psci_cpu_off(_cpuid: usize) {
     const PSCI_POWER_STATE_TYPE_POWER_DOWN: u32 = 1;
     const PSCI_0_2_POWER_STATE_TYPE_SHIFT: u32 = 16;
     let state: u32 = PSCI_POWER_STATE_TYPE_POWER_DOWN << PSCI_0_2_POWER_STATE_TYPE_SHIFT;
-    psci_0_1_cpu_off(PSCI_0_2_FN_CPU_OFF, state);
+    let _ = psci_call(PSCI_0_2_FN_CPU_OFF as usize, state as usize, 0, 0);
 }
 
-pub fn invoke_psci_fn(fn_id: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
-    psci_call(fn_id, arg0, arg1, arg2)
-}
-
-pub fn psci_0_1_cpu_on(fn_ids: u32, cpuid: usize, entry_point: usize, arg: usize) -> i32 {
-    let err: i32 = invoke_psci_fn(fn_ids as usize, cpuid, entry_point, arg) as i32;
-    psci_errno_tran(err)
-}
-
-pub fn psci_0_1_cpu_off(fn_ids: u32, state: u32) -> i32 {
-    let err: i32 = invoke_psci_fn(fn_ids as usize, state as usize, 0, 0) as i32;
-    psci_errno_tran(err)
-}
-
-pub fn psci_0_1_cpu_suspend(fn_ids: u32, state: u32, entry_point: usize) -> i32 {
-    let err: i32 = invoke_psci_fn(fn_ids as usize, state as usize, entry_point, 0) as i32;
-    psci_errno_tran(err)
-}
-pub fn psci_0_1_migrate(fn_ids: u32, cpuid: usize) -> i32 {
-    let err: i32 = invoke_psci_fn(fn_ids as usize, cpuid, 0, 0) as i32;
-    psci_errno_tran(err)
-}
-
-pub fn psci_errno_tran(errno: i32) -> i32 {
+pub fn psci_errno_tran(errno: i32) -> PsciRetNo {
     //EINVAL
     let mut ret = -22;
     let errno_tran = match errno {
         0 => {
             ret = 0;
-            PsciRetNo::PSCI_RET_SUCCESS
+            PsciRetNo::PsciRetSuccess
         }
         // EOPNOTSUPP
         -1 => {
             ret = -95;
-            PsciRetNo::PSCI_RET_NOT_SUPPORTED
+            PsciRetNo::PsciRetNotSupported
         }
         -2 => {
             ret = -22;
-            PsciRetNo::PSCI_RET_INVALID_PARAMS
+            PsciRetNo::PsciRetInvalidParams
         }
         // EPERM
         -3 => {
             ret = -1;
-            PsciRetNo::PSCI_RET_DENIED
+            PsciRetNo::PsciRetDenied
         }
-        -4 => PsciRetNo::PSCI_RET_ALREADY_ON,
-        -5 => PsciRetNo::PSCI_RET_ON_PENDING,
-        -6 => PsciRetNo::PSCI_RET_INTERNAL_FAILURE,
-        -7 => PsciRetNo::PSCI_RET_NOT_PRESENT,
-        -8 => PsciRetNo::PSCI_RET_DISABLED,
+        -4 => PsciRetNo::PsciRetAlreadyOn,
+        -5 => PsciRetNo::PsciRetOnPending,
+        -6 => PsciRetNo::PsciRetInternalFailure,
+        -7 => PsciRetNo::PsciRetNotPresent,
+        -8 => PsciRetNo::PsciRetDisabled,
         -9 => {
             ret = -22;
-            PsciRetNo::PSCI_RET_INVALID_ADDRESS
+            PsciRetNo::PsciRetInvalidAddress
         }
         _ => {
             warn!("Unknown psci errno: {}", errno);
-            PsciRetNo::PSCI_RET_INVALID_PARAMS
+            PsciRetNo::PsciRetInvalidParams
         }
     };
-    info!("return: {:?}", errno_tran);
-    ret
+    info!("return={}, {:?}", ret, errno_tran);
+    errno_tran
 }
 
 /// PSCI return values, inclusive of all PSCI versions
-#[allow(non_camel_case_types)]
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub enum PsciRetNo {
-    PSCI_RET_SUCCESS = 0,
-    PSCI_RET_NOT_SUPPORTED = -1,
-    PSCI_RET_INVALID_PARAMS = -2,
-    PSCI_RET_DENIED = -3,
-    PSCI_RET_ALREADY_ON = -4,
-    PSCI_RET_ON_PENDING = -5,
-    PSCI_RET_INTERNAL_FAILURE = -6,
-    PSCI_RET_NOT_PRESENT = -7,
-    PSCI_RET_DISABLED = -8,
-    PSCI_RET_INVALID_ADDRESS = -9,
+    PsciRetSuccess = 0,
+    PsciRetNotSupported = -1,
+    PsciRetInvalidParams = -2,
+    PsciRetDenied = -3,
+    PsciRetAlreadyOn = -4,
+    PsciRetOnPending = -5,
+    PsciRetInternalFailure = -6,
+    PsciRetNotPresent = -7,
+    PsciRetDisabled = -8,
+    PsciRetInvalidAddress = -9,
 }
