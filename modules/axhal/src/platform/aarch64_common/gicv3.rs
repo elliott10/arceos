1ad470f8b271adeba3ed142878e66315435c5237
aarkegz <aarkegz@gmail.com>
2025-05-26 15:31:40 +0800

    `inject_interrupt` on GICv3 based on hVisor

--- modules/axhal/src/platform/aarch64_common/gicv3.rs
+++ modules/axhal/src/platform/aarch64_common/gicv3.rs
@@ -1,11 +1,12 @@
 use crate::{arch::disable_irqs, irq::IrqHandler, mem::phys_to_virt};
 use arm_gic_driver::*;
 use axconfig::devices::{GICC_PADDR, GICD_PADDR, GICR_PADDR, UART_IRQ};
-use core::ptr::NonNull;
+use core::{panic, ptr::NonNull};
 use kspin::SpinNoIrq;
 use memory_addr::PhysAddr;
-use arm_gicv2::InterruptType;
-use arm_gicv2::translate_irq;
+use arm_gicv2::{translate_irq, InterruptType};
+#[cfg(feature = "hv")]
+use arm_gicv2::GicHypervisorInterface;
 
 /// The maximum number of IRQs.
 pub const MAX_IRQ_COUNT: usize = 1024;
@@ -27,7 +28,7 @@ const GICD_BASE: PhysAddr = pa!(GICD_PADDR);
 const GICR_BASE: PhysAddr = pa!(GICR_PADDR);
 
 static GICD: SpinNoIrq<Option<arm_gic_driver::v3::Gic>> = SpinNoIrq::new(None);
-static GICC: SpinNoIrq<Option<Box<dyn InterfaceCPU>>> = SpinNoIrq::new(None);
+static GICR: SpinNoIrq<Option<Box<dyn InterfaceCPU>>> = SpinNoIrq::new(None);
 
 /// Enables or disables the given IRQ.
 pub fn set_enable(irq_num: usize, enabled: bool) {
@@ -53,7 +54,7 @@ pub fn register_handler(irq_num: usize, handler: IrqHandler) -> bool {
 
 /// Fetches the IRQ number.
 pub fn fetch_irq() -> usize {
-    GICC.lock()
+    GICR.lock()
         .as_mut()
         .unwrap()
         .ack()
@@ -69,16 +70,144 @@ pub fn fetch_irq() -> usize {
 pub fn dispatch_irq(irq_num: usize) {
     let intid: Option<IrqId>;
     if irq_num == 0 {
-        intid = GICC.lock().as_mut().unwrap().ack();
+        intid = GICR.lock().as_mut().unwrap().ack();
         info!("interrupt {:?}", intid.unwrap());
     } else {
         intid = Some(IrqId::from(irq_num));
     }
     if let Some(intid) = intid {
         crate::irq::dispatch_irq_common(intid.into());
-        GICC.lock().as_mut().unwrap().eoi(intid);
+        GICR.lock().as_mut().unwrap().eoi(intid);
     }
 }
+
+/// Reads and returns the value of the given aarch64 system register.
+macro_rules! read_sysreg {
+    ($name:ident) => {
+        {
+            let mut value: u64;
+            unsafe{::core::arch::asm!(
+                concat!("mrs {value:x}, ", ::core::stringify!($name)),
+                value = out(reg) value,
+                options(nomem, nostack),
+            );}
+            value
+        }
+    }
+}
+
+/// Writes the given value to the given aarch64 system register.
+macro_rules! write_sysreg {
+    ($name:ident, $value:expr) => {
+        {
+            let v: u64 = $value;
+            unsafe{::core::arch::asm!(
+                concat!("msr ", ::core::stringify!($name), ", {value:x}"),
+                value = in(reg) v,
+                options(nomem, nostack),
+            )}
+        }
+    }
+}
+
+#[cfg(feature = "hv")]
+pub fn inject_interrupt(vector: usize) {
+    // mask
+    const LR_VIRTIRQ_MASK: usize = (1 << 32) - 1;
+
+    let elsr: u64 = read_sysreg!(ich_elrsr_el2);
+    let vtr = read_sysreg!(ich_vtr_el2) as usize;
+    let lr_num: usize = (vtr & 0xf) + 1;
+    let mut free_lr = -1 as isize;
+    for i in 0..lr_num {
+        // find a free list register
+        if (1 << i) & elsr > 0 {
+            if free_lr == -1 {
+                free_lr = i as isize;
+            }
+            continue;
+        }
+        let lr_val = read_lr(i) as usize;
+        // if a virtual interrupt is enabled and equals to the physical interrupt irq_id
+        if (lr_val & LR_VIRTIRQ_MASK) == vector {
+            trace!("virtual irq {} enables again", vector);
+        }
+    }
+    trace!("To Inject IRQ {}, find lr {}", vector, free_lr);
+
+    if free_lr == -1 {
+        panic!("No free list register to inject IRQ {}", vector);
+    } else {
+        let mut val = vector as u64; // vector
+        val |= 1 << 60; // group 1
+        val |= 1 << 62; // state pending
+        // hardware interrupt not supported
+        write_lr(free_lr as usize, val);
+    }
+}
+
+fn read_lr(id: usize) -> u64 {
+    let id = id as u64;
+    match id {
+        //TODO get lr size from gic reg
+        0 => read_sysreg!(ich_lr0_el2),
+        1 => read_sysreg!(ich_lr1_el2),
+        2 => read_sysreg!(ich_lr2_el2),
+        3 => read_sysreg!(ich_lr3_el2),
+        4 => read_sysreg!(ich_lr4_el2),
+        5 => read_sysreg!(ich_lr5_el2),
+        6 => read_sysreg!(ich_lr6_el2),
+        7 => read_sysreg!(ich_lr7_el2),
+        8 => read_sysreg!(ich_lr8_el2),
+        9 => read_sysreg!(ich_lr9_el2),
+        10 => read_sysreg!(ich_lr10_el2),
+        11 => read_sysreg!(ich_lr11_el2),
+        12 => read_sysreg!(ich_lr12_el2),
+        13 => read_sysreg!(ich_lr13_el2),
+        14 => read_sysreg!(ich_lr14_el2),
+        15 => read_sysreg!(ich_lr15_el2),
+        _ => {
+            panic!("invalid lr id {}", id);
+        }
+    }
+}
+
+fn write_lr(id: usize, val: u64) {
+    let id = id as u64;
+    match id {
+        0 => write_sysreg!(ich_lr0_el2, val),
+        1 => write_sysreg!(ich_lr1_el2, val),
+        2 => write_sysreg!(ich_lr2_el2, val),
+        3 => write_sysreg!(ich_lr3_el2, val),
+        4 => write_sysreg!(ich_lr4_el2, val),
+        5 => write_sysreg!(ich_lr5_el2, val),
+        6 => write_sysreg!(ich_lr6_el2, val),
+        7 => write_sysreg!(ich_lr7_el2, val),
+        8 => write_sysreg!(ich_lr8_el2, val),
+        9 => write_sysreg!(ich_lr9_el2, val),
+        10 => write_sysreg!(ich_lr10_el2, val),
+        11 => write_sysreg!(ich_lr11_el2, val),
+        12 => write_sysreg!(ich_lr12_el2, val),
+        13 => write_sysreg!(ich_lr13_el2, val),
+        14 => write_sysreg!(ich_lr14_el2, val),
+        15 => write_sysreg!(ich_lr15_el2, val),
+        _ => {
+            panic!("invalid lr id {}", id);
+        }
+    }
+}
+
+/// Sends Software Generated Interrupt (SGI)(s) (usually IPI) to the given dest CPU.
+pub fn send_sgi_one(dest_cpu_id: usize, irq_num: usize) {
+    todo!()
+}
+
+/// Sends a broadcast IPI to all CPUs.
+pub fn send_sgi_all(irq_num: usize) {
+    todo!()
+}
+
+pub struct MyVgic{}
 
 /// Initializes GICD, GICC on the primary CPU.
 pub(crate) fn init_primary() {
@@ -91,7 +220,7 @@ pub(crate) fn init_primary() {
     let interface = gicd.cpu_interface();
 
     GICD.lock().replace(gicd);
-    GICC.lock().replace(interface);
+    GICR.lock().replace(interface);
 
     disable_irqs();
 }
@@ -100,6 +229,6 @@ pub(crate) fn init_primary() {
 #[cfg(feature = "smp")]
 pub(crate) fn init_secondary() {
     let interface = GICD.lock().as_mut().unwrap().cpu_interface();
-    GICC.lock().replace(interface);
-    GICC.lock().as_mut().unwrap().setup();
+    GICR.lock().replace(interface);
+    GICR.lock().as_mut().unwrap().setup();
 }
